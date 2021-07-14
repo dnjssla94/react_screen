2f1bdbdc49d1111920f11394b7402827bcd03b1d
Kim Shinwon <dnjssla94@naver.com>
2021-07-14 15:10:51 +0900

    modify spacing

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,30 @@
 import './App.css';
 import React, { Component } from 'react';
 import ComboBox from './components/comboBox';
-import Grid from './components/grid';
-import RadioButton from './components/radioButton';
-import TextBox from './components/textBox';
+import Grid from './components/contentTable';
+
+const result = {
+  insuranceName: '',
+  insurancePeriod: '',
+  paymentPeriod: '',
+  paymentCycle: '',
+  paymentAmount: '',
+}
 
 class App extends Component {
-
   state = {
     comboBoxVal : "",
-    radioBtnVal : "",
-    textBoxVal : "",
     bankId: "",
-    insurance: [],
+    insuranceId: "",
+    insurances: [],
+    insuranceName:"",
+    insurancePeriod: "",
+    paymentPeriod: '',
+    paymentCycle: '',
+    paymentAmount: '',
+    insurancePeriods: [],
+    paymentPeriods: [],
+    paymentCycles: [],
     bankList : [
       {id:'km',val:'국민은행'},
       {id:'wr',val:'우리은행'},
@@ -25,78 +37,273 @@ class App extends Component {
       {id:'2',val:'무배당 암걱정없는표적치료암보험(갱신형) 프리미엄형'},
     ],
     wrInsuranceList : [
-      {id:'1',val:'무배당 라이나THE치아보험_보존강화형'},
-      {id:'2',val:'무배당 라이나THE치아보험_보철강화형'},
+      {id:'3',val:'무배당 라이나THE치아보험_보존강화형'},
+      {id:'4',val:'무배당 라이나THE치아보험_보철강화형'},
     ],
     shInsuranceList : [
-      {id:'1',val:'무배당 라이나간병플러스치매보험1종(페이퍼형) 분할지급'},
-      {id:'2',val:'무배당 라이나간병플러스치매보험1종(페이퍼형) 일시지급'},
+      {id:'5',val:'무배당 라이나간병플러스치매보험1종(페이퍼형) 분할지급'},
+      {id:'6',val:'무배당 라이나간병플러스치매보험1종(페이퍼형) 일시지급'},
     ],
     hnInsuranceList : [
-      {id:'1',val:'무배당 라이나간병플러스치매보험2종(순수보장형) 실속형'},
-      {id:'2',val:'무배당 라이나간병플러스치매보험2종(순수보장형) 프리미엄형'},
+      {id:'7',val:'무배당 라이나간병플러스치매보험2종(순수보장형) 실속형'},
+      {id:'8',val:'무배당 라이나간병플러스치매보험2종(순수보장형) 프리미엄형'},
     ],
+    kmInsurance1 : 
+      {
+        id:'1', 
+        insuranceName:"무배당 암걱정없는표적치료암보험(갱신형) 실속형",
+        insurancePeriods:[{id:'1',val:'10년'},{id:'2',val:'20년'}],
+        paymentPeriods:[{id:'1',val:'5년'},{id:'2',val:'10년'}],
+        paymentCycles:[{id:'1',val:'1개월'},{id:'3',val:'1년'}],
+      },
+    kmInsurance2 : 
+      { 
+        id:'2', 
+        insuranceName:"무배당 암걱정없는표적치료암보험(갱신형) 프리미엄형",
+        insurancePeriods:[{id:'1',val:'10년'},{id:'2',val:'20년'},{id:'3',val:'30년'}],
+        paymentPeriods:[{id:'1',val:'10년'},{id:'3',val:'20년'}],
+        paymentCycles:[{id:'2',val:'6개월'},{id:'3',val:'1년'}],
+      },
+    wrInsurance1 : 
+      {
+        id:'3', 
+        insuranceName:"무배당 라이나THE치아보험_보존강화형",
+        insurancePeriods:[{id:'1',val:'10년'},{id:'2',val:'20년'}],
+        paymentPeriods:[{id:'1',val:'5년'},{id:'2',val:'10년'}],
+        paymentCycles:[{id:'1',val:'1개월'},{id:'3',val:'1년'}],
+      },
+    wrInsurance2 : 
+    {
+      id:'4', 
+      insuranceName:"무배당 라이나THE치아보험_보철강화형",
+      insurancePeriods:[{id:'1',val:'10년'},{id:'2',val:'20년'},{id:'3',val:'30년'}],
+      paymentPeriods:[{id:'1',val:'10년'},{id:'3',val:'20년'}],
+      paymentCycles:[{id:'2',val:'6개월'},{id:'3',val:'1년'}],
+    },
+    shInsurance1 : 
+    {
+      id:'5', 
+      insuranceName:"무배당 라이나간병플러스치매보험1종(페이퍼형) 분할지급",
+      insurancePeriods:[{id:'1',val:'10년'},{id:'2',val:'20년'}],
+      paymentPeriods:[{id:'1',val:'5년'},{id:'2',val:'10년'}],
+      paymentCycles:[{id:'1',val:'1개월'},{id:'3',val:'1년'}],
+    },
+    shInsurance2 : 
+    {
+      id:'6', 
+      insuranceName:"무배당 라이나간병플러스치매보험1종(페이퍼형) 일시지급",
+      insurancePeriods:[{id:'1',val:'10년'},{id:'2',val:'20년'},{id:'3',val:'30년'}],
+      paymentPeriods:[{id:'2',val:'10년'},{id:'3',val:'20년'}],
+      paymentCycles:[{id:'2',val:'6개월'},{id:'3',val:'1년'}],
+    },
+    hnInsurance1 : 
+    {
+      id:'7', 
+      insuranceName:"무배당 라이나간병플러스치매보험2종(순수보장형) 실속형",
+      insurancePeriods:[{id:'1',val:'10년'},{id:'2',val:'20년'}],
+      paymentPeriods:[{id:'1',val:'5년'},{id:'2',val:'10년'}],
+      paymentCycles:[{id:'1',val:'1개월'},{id:'3',val:'1년'}],
+    },
+    hnInsurance2 : 
+    {
+      id:'8', 
+      insuranceName:"무배당 라이나간병플러스치매보험2종(순수보장형) 프리미엄형",
+      insurancePeriods:[{id:'1',val:'10년'},{id:'2',val:'20년'},{id:'3',val:'30년'}],
+      paymentPeriods:[{id:'2',val:'10년'},{id:'3',val:'20년'}],
+      paymentCycles:[{id:'2',val:'6개월'},{id:'3',val:'1년'}],
+    },
   }
 
   getBankId = (value) => {
-    this.setState({...this.state,bankId: value});
+    this.setState({...this.state, bankId: value});
     switch(value) {
       case 'km':
-        this.setState({...this.state, insurance: this.state.kmInsuranceList});
+        this.setState({...this.state, insurances: this.state.kmInsuranceList});
         break;
       case 'wr':
-        this.setState({...this.state, insurance: this.state.wrInsuranceList});
+        this.setState({...this.state, insurances: this.state.wrInsuranceList});
         break;
       case 'sh':
-        this.setState({...this.state, insurance: this.state.shInsuranceList});
+        this.setState({...this.state, insurances: this.state.shInsuranceList});
         break;
       case 'hn':
-        this.setState({...this.state, insurance: this.state.hnInsuranceList});
+        this.setState({...this.state, insurances: this.state.hnInsuranceList});
         break;
     }
   }
+  getInsuranceId = (value) => {
+    this.setState({...this.state,insuranceId:value});
+    switch(value){
+      case '1':
+        this.setState({...this.state, 
+          insuranceName: this.state.kmInsurance1.insuranceName,
+          insurancePeriods:this.state.kmInsurance1.insurancePeriods,
+          paymentPeriods: this.state.kmInsurance1.paymentPeriods,
+          paymentCycles: this.state.kmInsurance1.paymentCycles,
+        });
+        break;
+      case '2':
+        this.setState({...this.state, 
+          insuranceName: this.state.kmInsurance2.insuranceName,
+          insurancePeriods:this.state.kmInsurance2.insurancePeriods,
+          paymentPeriods: this.state.kmInsurance2.paymentPeriods,
+          paymentCycles: this.state.kmInsurance2.paymentCycles,
+        });
+        break;
+      case '3':
+        this.setState({...this.state, 
+          insuranceName: this.state.wrInsurance1.insuranceName,
+          insurancePeriods:this.state.wrInsurance1.insurancePeriods,
+          paymentPeriods: this.state.wrInsurance1.paymentPeriods,
+          paymentCycles: this.state.wrInsurance1.paymentCycles,
+        });
+        break;
+      case '4':
+        this.setState({...this.state, 
+          insuranceName: this.state.wrInsurance2.insuranceName,
+          insurancePeriods:this.state.wrInsurance2.insurancePeriods,
+          paymentPeriods: this.state.wrInsurance2.paymentPeriods,
+          paymentCycles: this.state.wrInsurance2.paymentCycles,
+        });
+        break;
+      case '5':
+        this.setState({...this.state, 
+          insuranceName: this.state.shInsurance1.insuranceName,
+          insurancePeriods:this.state.shInsurance1.insurancePeriods,
+          paymentPeriods: this.state.shInsurance1.paymentPeriods,
+          paymentCycles: this.state.shInsurance1.paymentCycles,
+        });
+        break;
+      case '6':
+        this.setState({...this.state, 
+          insuranceName: this.state.shInsurance2.insuranceName,
+          insurancePeriods:this.state.shInsurance2.insurancePeriods,
+          paymentPeriods: this.state.shInsurance2.paymentPeriods,
+          paymentCycles: this.state.shInsurance2.paymentCycles,
+        });
+        break;
+      case '7':
+        this.setState({...this.state, 
+          insuranceName: this.state.hnInsurance1.insuranceName,
+          insurancePeriods:this.state.hnInsurance1.insurancePeriods,
+          paymentPeriods: this.state.hnInsurance1.paymentPeriods,
+          paymentCycles: this.state.hnInsurance1.paymentCycles,
+        });
+        break;
+      case '8':
+        this.setState({...this.state, 
+          insuranceName: this.state.hnInsurance2.insuranceName,
+          insurancePeriods:this.state.hnInsurance2.insurancePeriods,
+          paymentPeriods: this.state.hnInsurance2.paymentPeriods,
+          paymentCycles: this.state.hnInsurance2.paymentCycles,
+        });
+        break;
+    }
+  }
+  getInsurancePeriod = (value) => {
+    switch(value) {
+      case '1':
+        this.setState({...this.state,insurancePeriod:'10년' });
+        break;
+      case '2':
+        this.setState({...this.state,insurancePeriod:'20년' });
+        break;
+      case '3':
+        this.setState({...this.state,insurancePeriod:'30년' });
+        break;
+    }
+
+  }
+  getPaymentPeriod = (value) => {
+    switch(value) {
+      case '1':
+        this.setState({...this.state,paymentPeriod:'5년' });
+        break;
+      case '2':
+        this.setState({...this.state,paymentPeriod:'10년' });
+        break;
+      case '3':
+        this.setState({...this.state,paymentPeriod:'20년' });
+        break;
+    }
+  }
+  getPaymentCycle = (value) => {
+    switch(value) {
+      case '1':
+        this.setState({...this.state,paymentCycle:'1개월' });
+        break;
+      case '2':
+        this.setState({...this.state,paymentCycle:'6개월' });
+        break;
+      case '3':
+        this.setState({...this.state,paymentCycle:'1년' });
+        break;
+    }
+  }
+  getPaymentAmount = (value) => {
+    this.setState({...this.state, paymentAmount: value})
+  }
+
+  
   getComboBoxVal = (value) => {
     this.setState({...this.state,comboBoxVal: value});
   };
-  getRadioBtnVal = (value) => {
-    this.setState({...this.state,radioBtnVal: value});
-  };
-  getTextBoxVal = (value) => {
-    this.setState({...this.state,textBoxVal: value});
-  };
-  dataCheck = () => {
-    this.getComboBoxVal();
-    this.getRadioBtnVal();
-    this.getTextBoxVal();
-    console.log(this.state);
-  };
+
+  insuranceDataCheck = () => {
+    result.insuranceName = this.state.insuranceName;
+    result.insurancePeriod = this.state.insurancePeriod;
+    result.paymentPeriod = this.state.paymentPeriod;
+    result.paymentCycle = this.state.paymentCycle;
+    result.paymentAmount = this.state.paymentAmount
+    console.log(result);
+  }
 
   render() {
     return (
       <>
-        <Grid></Grid>
+        
+        <table>
+          <thead>
+            <tr>
+              <td>금융기관</td>
+              <td>
+                <ComboBox 
+                  dataList={this.state.bankList}  
+                  getComboBoxVal={this.getBankId}>
+                </ComboBox>
+              </td>
+              <td>상품명</td>
+              <td>
+                <ComboBox 
+                  dataList={this.state.insurances}  
+                  getComboBoxVal={this.getInsuranceId}>
+                </ComboBox>
+              </td>
+            </tr>
+          </thead>
+        </table>
+
         <div className="space"></div> 
-        <ComboBox getComboBoxVal={this.getComboBoxVal}></ComboBox>
-        <div className="space"></div> 
-        <RadioButton getRadioBtnVal={this.getRadioBtnVal}></RadioButton>
-        <div className="space"></div> 
-        <div>name: </div>
-        <TextBox getTextBoxVal={this.getTextBoxVal}></TextBox>
-        <div className="space"></div> 
-        <button onClick={this.dataCheck}>Check Data</button>
+
         <div>
-          <div className="textBox">금융 기관 </div>
-          <ComboBox 
-            dataList={this.state.bankList}  
-            getComboBoxVal={this.getBankId}></ComboBox>
-          <div className="textBox">상품명 </div>
-          <ComboBox 
-            dataList={this.state.insurance}  
-            getComboBoxVal={this.getBankId}></ComboBox>
+          <Grid
+            insuranceName={this.state.insuranceName}
+            insurancePeriods={this.state.insurancePeriods}
+            paymentPeriods={this.state.paymentPeriods}
+            paymentCycles={this.state.paymentCycles}
+            getInsurancePeriod={this.getInsurancePeriod}
+            getPaymentPeriod={this.getPaymentPeriod}
+            getPaymentCycle={this.getPaymentCycle}
+            getPaymentAmount={this.getPaymentAmount}>
+          </Grid>
         </div>
+
+        <div className="space"></div> 
+
+        <button onClick={this.insuranceDataCheck}>Insurance Data Check</button>
       </>
     );
   }
-}
+};
 
 export default App;
--- src/components/textBox.jsx
+++ src/components/textBox.jsx
@@ -9,7 +9,6 @@ class TextBox extends Component {
         
     }
     
-
     render() {
         return (
             <input 
